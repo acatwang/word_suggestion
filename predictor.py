f49560f7d85cbc0fd3970c4960d99597fdd3a2c7
Yi-Yin Wang <acatwang@gmail.com>
2014-12-07 15:02:01 -0500

    Use HMM/MLE to find best POS sequence

--- predictor.py
+++ predictor.py
@@ -115,3 +115,139 @@ print cpd_tags["PRON"].prob("VERB")
 print "The probability of the tag sequence 'START PRO V TO V END' for 'I want to race' is:", prob_tagsequence
 
 
+""" Viterbi
+If we have a word sequence, what is the best tag sequence?
+
+The method above lets us determine the probability for a single tag sequence.
+But in order to find the best tag sequence, we need the probability for _all_ tag sequence.
+What Viterbi gives us is just a good way of computing all those many probabilities
+as fast as possible.
+"""
+distinct_tags = set(brown_tags)
+
+# Given a sentence
+sentence = ["I", "want", "to", "race"]
+sentlen = len(sentence)
+
+# viterbi: this is a list.
+# for each step i in 1 .. sentlen,
+# store a dictionary that maps each tag X
+# to the probability of the best tag sequence of length i that ends in X
+viterbi = []
+
+# backpointer:
+# for each step i in 1..sentlen,
+# store a dictionary
+# that maps each tag X
+# to the previous tag in the best tag sequence of length i that ends in X
+backpointer = []
+
+##
+# we first determine the viterby dictionary for the first word:
+# For each tag, what is the probability of it following "START" and for it
+# producing the first word of the sentence?
+first_viterbi = { }
+first_backpointer = { }
+for tag in distinct_tags:
+    # don't record anything for the START tag
+    if tag == "START": continue
+
+    first_viterbi[ tag ] = cpd_tags["START"].prob(tag) * cpd_tagwords[tag].prob( sentence[0] )
+    first_backpointer[ tag ] = "START"
+
+# store first_viterbi (the dictionary for the first word in the sentence)
+# in the viterbi list, and record that the best previous tag
+# for any first tag is "START"
+viterbi.append(first_viterbi)
+backpointer.append(first_backpointer)
+
+
+# now we iterate over all remaining words in the sentence, from the second to the last.
+for wordindex in range(1, len(sentence)):
+         # start a new dictionary where we can store, for each tag, the probability
+     # of the best tag sequence ending in that tag
+         # for the current word in the sentence
+    this_viterbi = { }
+         # start a new dictionary we we can store, for each tag,
+    # the best previous tag
+    this_backpointer = { }
+         # prev_viterbi is a dictionary that stores, for each tag, the probability
+    # of the best tag sequence ending in that tag
+    # for the previous word in the sentence.
+    # So it stores, for each tag, the probability of a tag sequence up to the previous word
+    # ending in that tag.
+    prev_viterbi = viterbi[-1]
+
+    # for each tag, determine what the best previous-tag is,
+    # and what the probability is of the best tag sequence ending in this tag.
+    # store this information in the dictionary this_viterbi
+    for tag in distinct_tags:
+        # don't record anything for the START tag
+        if tag == "START": continue
+
+        # if this tag is X and the current word is w, then
+        # find the previous tag Y such that
+        # the best tag sequence that ends in X
+        # actually ends in Y X
+        # that is, the Y that maximizes
+        # prev_viterbi[ Y ] * P(X | Y) * P( w | X)
+        # The following command has the same notation
+        # that you saw in the sorted() command.
+        best_previous = max(prev_viterbi.keys(),
+                            key = lambda prevtag: \
+            prev_viterbi[ prevtag ] * cpd_tags[prevtag].prob(tag) * cpd_tagwords[tag].prob(sentence[wordindex]))
+
+        # Instead, we can also use the following longer code:
+        # best_previous = None
+        # best_prob = 0.0
+        # for prevtag in prev_viterbi.keys():
+        #    prob = prev_viterbi[ prevtag ] * cpd_tags[prevtag].prob(tag) * cpd_tagwords[tag].prob(sentence[wordindex])
+        #    if prob >= best_prob:
+        #        best_previous= prevtag
+        #        best_prob = prob
+        #
+                  # this_viterbi[ tag ] is the probability of the best tag sequence ending in tag
+        this_viterbi[ tag ] = prev_viterbi[ best_previous] * \
+            cpd_tags[ best_previous ].prob(tag) * cpd_tagwords[ tag].prob(sentence[wordindex])
+                 # this_backpointer[ tag ] is the most likely previous-tag for this current tag
+        this_backpointer[ tag ] = best_previous
+
+        # done with all tags in this iteration
+    # so store the current viterbi step
+    viterbi.append(this_viterbi)
+    backpointer.append(this_backpointer)
+
+
+# done with all words in the sentence.
+# now find the probability of each tag
+# to have "END" as the next tag,
+# and use that to find the overall best sequence
+prev_viterbi = viterbi[-1]
+best_previous = max(prev_viterbi.keys(),
+                    key = lambda prevtag: prev_viterbi[ prevtag ] * cpd_tags[prevtag].prob("END"))
+
+prob_tagsequence = prev_viterbi[ best_previous ] * cpd_tags[ best_previous].prob("END")
+
+# best tagsequence: we store this in reverse for now, will invert later
+best_tagsequence = [ "END", best_previous ]
+# invert the list of backpointers
+backpointer.reverse()
+
+# go backwards through the list of backpointers
+# (or in this case forward, because we have inverter the backpointer list)
+# in each case:
+# the following best tag is the one listed under
+# the backpointer for the current best tag
+current_best_tag = best_previous
+for bp in backpointer:
+    best_tagsequence.append(bp[current_best_tag])
+    current_best_tag = bp[current_best_tag]
+
+best_tagsequence.reverse()
+print "The sentence was:",
+for w in sentence: print w,
+print
+print "The best tag sequence is:",
+for t in best_tagsequence: print t,
+print
+print "The probability of the best tag sequence is:", prob_tagsequence
