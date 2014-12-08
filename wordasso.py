4f1171f494717521e3e6ba93df561fe42493096f
Sang-Jung Han <hansj@umich.edu>
2014-12-08 14:39:33 -0500

    improved

--- wordasso.py
+++ wordasso.py
@@ -20,8 +20,20 @@ with open(in_file, 'rU') as csvfile:
 			tokens_all.append(token)
 
 finder = BigramCollocationFinder.from_words(tokens_all)
-finder.apply_freq_filter(3)
-print finder.nbest(bigram_measures.pmi, 10)
+# finder.apply_freq_filter(3)
+# print finder.nbest(bigram_measures.pmi, 10)
 
-
-
+more = True
+while more:
+	query_sentence = raw_input('Enter a sentence: ')
+	if query_sentence.lower() == 'no more':
+		print 'Good Bye!'
+		more = False
+	else:
+		query = query_sentence.split()[-1]
+		finder.apply_ngram_filter(lambda w1, w2: query not in (w1))
+		scored = finder.score_ngrams(bigram_measures.raw_freq)
+		results = sorted(finder.nbest(bigram_measures.raw_freq, 10))
+		finder = BigramCollocationFinder.from_words(tokens_all)
+		for w1, w2 in results:
+			print w2
